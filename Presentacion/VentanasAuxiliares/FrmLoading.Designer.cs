ba4ef11cea93e59bbc462041b0f8ab603e493217
arizacristal <cristalarizamartinez@gmail.com>
2023-10-14 16:10:56 -0500

    carga inicial del proyecto

---
+++ Presentacion/VentanasAuxiliares/FrmLoading.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Presentacion.VentanasAuxiliares
+{
+    partial class FrmLoading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Cargar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Presentacion.Properties.Resources.ImgLoading;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(764, 417);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Cargar
+            // 
+            this.Cargar.Location = new System.Drawing.Point(679, 410);
+            this.Cargar.Name = "Cargar";
+            this.Cargar.Size = new System.Drawing.Size(100, 23);
+            this.Cargar.TabIndex = 1;
+            this.Cargar.Visible = false;
+            // 
+            // FrmLoading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Cargar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmLoading";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmLoading";
+            this.Load += new System.EventHandler(this.FrmLoading_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar Cargar;
+    }
+}
---
+++ Presentacion/VentanasAuxiliares/FrmLoading.cs
@@ -0,0 +1,36 @@
+﻿using Presentacion.VentanasPrincipales;
+using System;
+using System.Windows.Forms;
+
+namespace Presentacion.VentanasAuxiliares
+{
+    public partial class FrmLoading : Form
+    {
+        public FrmLoading()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLoading_Load(object sender, EventArgs e)
+        {
+            this.timer1.Start();
+        }
+
+        private void Loading()
+        {
+            Cargar.Increment(1);
+            if (Cargar.Value == Cargar.Maximum)
+            {
+                this.timer1.Stop();
+                this.Hide();
+                FrmLogin frmLogin = new FrmLogin();
+                frmLogin.ShowDialog();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Loading();
+        }
+    }
+}
